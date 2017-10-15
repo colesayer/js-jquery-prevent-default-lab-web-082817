c9a66fd5d828a9c4144daffed97b657600086d1d
colesayer <colesayershapiro@gmail.com>
2017-10-15 14:54:53 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  $('form').on("submit", function(ev){
+    ev.preventDefault();
+    let input = $('#item').val()
+    addToDo(input)
+  })
 });
 
 // define functions here
+function addToDo(input){
+  $('#list ol').append(`<li>${input}</li>`)
+  $('form').trigger("reset");
+}
